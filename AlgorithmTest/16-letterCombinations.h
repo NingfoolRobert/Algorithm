f3a77837f09f2e71f1b5a2a4c2d78f80de80bea0
NingfoolRobert <371494724@qq.com>
2019-07-17 23:43:45 +0800

    1.add the letterCombinations

---
+++ AlgorithmTest/16-letterCombinations.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+#include <map>
+
+
+
+
+using namespace std;
+
+
+
+class SolutionletterCombinations
+{
+public:
+	std::vector<std::string> letterCombinations(std::string digits);
+protected:
+private:
+};
+
+std::vector<std::string> SolutionletterCombinations::letterCombinations(std::string digits)
+{
+	std::map<std::string, std::string> strMap;
+	strMap["0"] = "";
+	strMap["1"] = "";
+	strMap["2"] = "abc";
+	strMap["3"] = "def";
+	strMap["4"] = "ghi";
+	strMap["5"] = "jkl";
+	strMap["6"] = "mno";
+	strMap["7"] = "pqrs";
+	strMap["8"] = "tuv";
+	strMap["9"] = "wxyz";
+
+	std::vector<std::string> vecStr;
+	
+	int nSize = digits.length();
+	for (int iLoop = 0; iLoop < nSize; ++iLoop)
+	{
+		string strLetter;
+		string strTmp = digits.substr(iLoop, 1);
+		string str = strMap[strTmp];
+		int nlen = str.length();
+		strLetter += strTmp;
+		for (int i = 0; i < nlen; ++i)
+		{
+			
+		}
+
+	}
+}
+
