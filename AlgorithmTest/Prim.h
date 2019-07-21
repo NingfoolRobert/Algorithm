f008ade25e81af7664dcb79040beed3bbaf44651
NingfoolRobert <371494724@qq.com>
2019-07-21 17:15:40 +0800

    1.add the  file : Prim.h

---
+++ AlgorithmTest/Prim.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <vector>
+
+
+using namespace std;
+
+class CPrim
+{
+public:
+protected:
+private:
+};
+
+#define MAX_INT 999999
+pair<int, int> GetShortestEdge(const vector<vector<int> >& Graph, const vector<bool>& isIncluded)//求当前在MST之外距离MST最近的点的id
+{
+	int minID = -1;
+	int minDist = MAX_INT;
+	pair<int, int> minEdge;
+	for (int i = 0; i < Graph.size(); i++)//i为MST内的点
+	{
+		if (!isIncluded[i]) continue;//如果不在MST里面，则跳过
+		for (int j = 0; j < Graph.size(); j++) //j为MST外的点
+			if (!isIncluded[j] && Graph[i][j] < minDist) { //找到不在MST内但是距离MST最近的点
+				minID = j;
+				minDist = Graph[i][j];
+				minEdge = make_pair(i, j);
+			}
+	}
+	return minEdge;
+}
+vector<pair<int, int> > Prim(const vector<vector<int> >& Graph, vector<bool>& isIncluded) {
+	vector<pair<int, int> > MST;
+	isIncluded[0] = true;
+	//MST.push_back();
+	for (int i = 1; i < Graph.size(); i++) {
+		pair<int, int> minEdge = GetShortestEdge(Graph, isIncluded); //找到这次要放入的边i，j
+		MST.push_back(minEdge); //放入
+		isIncluded[minEdge.second] = true; //将j标记为已经放入
+	}
+	return MST;
+}
+
+void addEdge(const int& startP, const int& endP, const int& weight, vector<vector<int> >& Graph)
+{
+	Graph[startP][endP] = weight;
+	Graph[endP][startP] = weight;
+}
+
+int Test()
+{
+	int vertex_num = 6;
+	vector<vector<int> > Graph(vertex_num, vector<int>(vertex_num, MAX_INT));
+	addEdge(0, 1, 6, Graph);
+	addEdge(0, 2, 1, Graph);
+	addEdge(0, 3, 5, Graph);
+	addEdge(1, 2, 5, Graph);
+	addEdge(1, 4, 3, Graph);
+	addEdge(2, 3, 5, Graph);
+	addEdge(2, 4, 6, Graph);
+	addEdge(2, 5, 4, Graph);
+	addEdge(3, 5, 2, Graph);
+	addEdge(4, 5, 6, Graph);
+	vector<bool> isIncluded(vertex_num, false);
+	vector<pair<int, int> >  MST = Prim(Graph, isIncluded);
+	for (int i = 0; i < MST.size(); i++) //按照放入MST的顺序依次输出
+		cout << MST[i].first + 1 << "->" << MST[i].second + 1 << endl;
+	return 0;
+}
