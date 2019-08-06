842c23116d629bdce44217103a6f38e3853dec5d
NingfoolRobert <371494724@qq.com>
2019-08-06 19:11:38 +0800

    1. add the file: 114-flatten.h

---
+++ AlgorithmTest/114-flatten.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "TreeDefine.h"
+
+
+using	namespace std;
+
+
+class CSolution
+{
+public:
+	CSolution();
+	~CSolution();
+public:
+	void flatten(TreeNode* root);
+private:
+
+};
+
+CSolution::CSolution()
+{
+}
+
+CSolution::~CSolution()
+{
+}
+
+//将二叉树展开为列表
+void CSolution::flatten(TreeNode* root)
+{
+
+}
+
