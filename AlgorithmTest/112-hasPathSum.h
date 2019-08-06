f20b5474ca6b929454affd72d1c9149d40b33d87
NingfoolRobert <371494724@qq.com>
2019-08-06 10:06:55 +0800

    1. add the function： hasPathSum_stack

--- AlgorithmTest/112-hasPathSum.h
+++ AlgorithmTest/112-hasPathSum.h
@@ -2,8 +2,13 @@
 
 #include <iostream>
 #include <algorithm>
+#include <stack>
+#include <queue>
+
 #include "TreeDefine.h"
 
+
+using namespace std;
 
 class CSolution
 {
@@ -12,6 +17,7 @@ public:
 	~CSolution();
 public:
 	bool hasPathSum(TreeNode* root, int sum);
+	bool hasPathSum_stack(TreeNode* root, int sum);
 private:
 
 };
@@ -43,5 +49,28 @@ bool CSolution::hasPathSum(TreeNode* root, int sum)
 	if (0 == sum)
 		return true;
 	return hasPathSum(root->pLeft, sum) || hasPathSum(root->pRight, sum);
+}
+
+bool CSolution::hasPathSum_stack(TreeNode* root, int sum)
+{
+	if (root == nullptr)
+		return false;
+
+	stack<pair<TreeNode*, int>> stNode;
+
+	stNode.push(make_pair(root, sum - root->_value));
+	
+	while (!stNode.empty())
+	{
+		auto p = stNode.top();
+		stNode.pop();
+		if (p.first->pLeft == nullptr && p.first->pRight == nullptr && sum == 0)
+			return true;
+		if (sum == 0)
+			return true;
+		stNode.push(make_pair(p.first->pLeft, sum - p.first->pRight->_value));
+		stNode.push(make_pair(p.first->pRight, sum - p.first->pRight->_value));
+	}
+	return false;
 }
 
