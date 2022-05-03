845830b721dae00222076be93f66a89aab15eaf9
NingfoolRobert <371494724@qq.com>
2022-05-03 20:00:02 +0800

    add

--- AlgorithmTest/32-longestValidParentheses.h
+++ AlgorithmTest/32-longestValidParentheses.h
@@ -21,41 +21,42 @@ using namespace std;
 class Solution32 {
 public:
 	int longestValidParentheses(string s) {
-		stack<char> ss;
-		int nLong = 0;
-		int nCt = 0;
-		if (s.length() <= 1) return nLong;
-
-		char chTmp;
-		for (auto i = 0;i < s.length();++i)
-		{
-			if (ss.empty())
-			{
-				ss.push(s.at(i));
+		int maxans = 0;
+		stack<int> stk;
+		stk.push(-1);
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] == '(') {
+				stk.push(i);
 			}
-			else 
-			{
-				chTmp = ss.top();
-				if (chTmp == '(')
-				{
-					if (s.at(i) == ')')
-					{
-						ss.pop();
-						nCt++;
-						nLong = max(nCt, nLong);
-					}
-					else 
-					{
-						ss.push(s.at(i));
-					}
+			else {
+				stk.pop();
+				if (stk.empty()) {
+					stk.push(i);
 				}
-				else
-				{
-					ss.push(s.at(i));
-					nCt = 0;
+				else {
+					maxans = max(maxans, i - stk.top());
 				}
 			}
 		}
-		return nLong * 2;
+
+		return maxans;
+	}
+
+	int longestValidParentheses2(string s) {
+		vector<int>  dp(s.size(), 0);
+		int maxans = 0;
+		for (auto i = 1; i < s.size(); ++i)
+		{
+			if (s.at(i) == ')')
+			{
+				if (s.at(i - 1) == '(')
+					dp[i] = dp[i - 1] + 2;
+				else if(i - dp[i - 1] > 0 && s.at(i - dp[i - 1] - 1) == '(')
+					dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+
+				maxans = max(maxans, dp[i]);
+			}
+		}
+		return maxans;
 	}
 };
